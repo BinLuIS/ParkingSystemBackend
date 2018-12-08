2c091b458467dc525c0b97114db89b7bcfee9e3c
Connie Ip <connie.ip@cargosmart.com>
2018-12-08 19:46:04 +0800

    [Connie] fix: fix testcase db

--- src/test/java/com/binluis/parkingsystem/OrderResourceTest.java
+++ src/test/java/com/binluis/parkingsystem/OrderResourceTest.java
@@ -1,60 +1,51 @@
 package com.binluis.parkingsystem;
 
+import com.binluis.parkingsystem.domain.ParkingLot;
+import com.binluis.parkingsystem.domain.ParkingLotRepository;
 import com.binluis.parkingsystem.domain.ParkingOrder;
 import com.binluis.parkingsystem.domain.ParkingOrderRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import static com.binluis.parkingsystem.WebTestUtil.getContentAsObject;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class TestTest {
+
+public class OrderResourceTest {
     @Autowired
-    private MockMvc mvc;
+    ParkingOrderRepository parkingOrderRepository;
 
     @Autowired
-    private ParkingOrderRepository parkingOrderRepository;
-
-    @Test
-    public void should_get_message() throws Exception {
-        MvcResult result=this.mvc.perform(get("/test")).andExpect(status().isOk()).andReturn();
-        String json=result.getResponse().getContentAsString();
-
-        assertEquals("{test:success}",json);
-
-    }
-
-    @Test
-    public void should_get_message_for_lot_order_relationship() throws Exception {
-        MvcResult result=this.mvc.perform(get("/test/temp")).andExpect(status().isOk()).andReturn();
-        String json=result.getResponse().getContentAsString();
-
-        assertEquals("{test:success}",json);
-    }
+    MockMvc mvc;
 
     @Test
     public void should_get_all_orders() throws Exception {
         ParkingOrder parkingOrder = new ParkingOrder("car2","park","accept");
         parkingOrderRepository.save(parkingOrder);
         parkingOrderRepository.flush();
-        MvcResult result=this.mvc.perform(get("/orders")).andExpect(status().isOk()).andReturn();
-        String json=result.getResponse().getContentAsString();
 
-        assertEquals("{test:success}",json);
+        MvcResult result=this.mvc.perform(get("/orders")).andExpect(status().isOk()).andReturn();
+        final ParkingOrder[] parkingOrders = getContentAsObject(result, ParkingOrder[].class);
+
+
+        assertEquals(1,parkingOrders.length);
+        assertEquals("car2",parkingOrders[0].getCarNumber());
+        assertEquals("park",parkingOrders[0].getRequestType());
+        assertEquals("accept",parkingOrders[0].getStatus());
     }
 
 }
---
+++ src/test/java/com/binluis/parkingsystem/WebTestUtil.java
@@ -0,0 +1,17 @@
+package com.binluis.parkingsystem;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.IOException;
+
+public class WebTestUtil {
+    public static <T> T toObject(String jsonContent, Class<T> clazz) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(jsonContent, clazz);
+    }
+
+    public static <T> T getContentAsObject(MvcResult result, Class<T> clazz) throws Exception {
+        return toObject(result.getResponse().getContentAsString(), clazz);
+    }
+}

65e39f44701816e04dd9dc040fd56a4fa843883b
HOJO3 <joe.ho@oocl.com>
2018-12-08 12:17:50 +0800

    [JoeHo] feat: add annotation to recreate database for each test in unit test

--- src/test/java/com/binluis/parkingsystem/TestTest.java
+++ src/test/java/com/binluis/parkingsystem/TestTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Test;
 
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MvcResult;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class TestTest {
     @Autowired
     private MockMvc mvc;

c6fc8a54891734da9e326278bf2ead0b4b7157ea
Connie Ip <connie.ip@cargosmart.com>
2018-12-12 11:06:46 +0800

    [Connie] modify sql for roles

--- src/main/resources/db/migration/V7__add_parkingclerk_role.sql
+++ src/main/resources/db/migration/V7__add_parkingclerk_role.sql
@@ -2,4 +2,4 @@ alter table users add phone_number varchar(11);
 
 alter table users add idInRole bigint;
 
-insert into roles (name) values("PARKINGCLERK");
+insert into roles (name) values('PARKINGCLERK');

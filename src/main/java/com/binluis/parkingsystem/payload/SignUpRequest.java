1e70e7d05f0c0c53955ef3da9f17d8d53419d31c
Connie Ip <connie.ip@cargosmart.com>
2018-12-12 14:21:15 +0800

    [Connie] make signup request constraint loose

--- src/main/java/com/binluis/parkingsystem/payload/SignUpRequest.java
+++ src/main/java/com/binluis/parkingsystem/payload/SignUpRequest.java
@@ -4,23 +4,24 @@ import javax.validation.constraints.*;
 
 public class SignUpRequest {
     @NotBlank
-    @Size(min = 4, max = 40)
+    @Size(min = 1, max = 40)
     private String name;
 
     @NotBlank
-    @Size(min = 3, max = 15)
+    @Size(min = 1, max = 40)
     private String username;
 
     @NotBlank
     @Size(max = 40)
-    @Email
     private String email;
 
     @NotBlank
-    @Size(min = 6, max = 20)
+    @Size(min = 1, max = 20)
     private String password;
 
+    @Size(max = 11)
     private String phoneNumber;
+
     private String role;
 
     public String getName() {

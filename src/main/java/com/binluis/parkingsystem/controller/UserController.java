901dbb6565bd1520cbd9e00509c576ff77a2720a
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-14 01:28:13 +0800

    [Kyle] add encoder for user reset password

--- src/main/java/com/binluis/parkingsystem/controller/UserController.java
+++ src/main/java/com/binluis/parkingsystem/controller/UserController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -30,6 +31,8 @@ public class UserController {
     private ParkingBoyRepository parkingBoyRepository;
     @Autowired
     private ManagerRepository managerRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -168,7 +171,8 @@ public class UserController {
             user.get().setStatus(request.getStatus());
         }
         if(request.getPassword()!=null) {
-            user.get().setPassword(request.getPassword());
+            user.get().setPassword(passwordEncoder.encode(request.getPassword()));
+
         }
 
         userRepository.saveAndFlush(user.get());
